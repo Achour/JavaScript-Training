c512ef869419e0570841c3fd10c6ce69a74908ff
Achour Meguennui <achourphp@gmail.com>
2021-11-10 21:07:51 +0100

    Promises Example

---
+++ promises.js
@@ -0,0 +1,21 @@
+
+// a function to get a random number
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max) + 1;
+}
+
+// Promise example
+let p = new Promise((resolve, reject) => {
+    let a = getRandomInt(2)
+    if (a == 2) {
+        resolve('Success')
+    } else {
+        reject('Failed')
+    }
+})
+
+p.then((msg) => {
+    console.log(msg)
+}).catch((msg) => {
+    console.log(msg)
+})
